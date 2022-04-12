39341b97e108a38004abaa22c20d29b65102268e
karimw03 <103416014+karimw03@users.noreply.github.com>
2022-04-12 16:00:27 +0300

    Create cat.h

---
+++ cat.h
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "animal.h"
+using std::cout;
+
+class Cat :public Animal {
+private:
+	int color;
+	int pawSize;
+
+public:
+	Cat(int, int, int, int);
+	~Cat();
+	void print() {
+		Animal::print();
+		cout << "Cat print\n";
+	}
+	virtual void getOwner() { cout << "\nOwner: Cat"; }
+};

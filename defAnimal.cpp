58f8dadc07048dbe24df766ed05b1c82427f5df6
karimw03 <103416014+karimw03@users.noreply.github.com>
2022-04-12 16:16:27 +0300

    Update defAnimal.cpp

--- defAnimal.cpp
+++ defAnimal.cpp
@@ -11,13 +11,3 @@ Animal::Animal(int n, int a) {
 Animal::~Animal() {
 	cout << "Animal destroyed\n";
 }
-
-Fish::Fish(int n, int a, int gCap, int sS) :Animal(n, a) {
-	cout << "Fish created\n";
-	gillCapacity = gCap;
-	swimSpeed = sS;
-}
-
-Fish::~Fish() {
-	cout << "Fish destoryed\n";
-}
